f884487b6dff2092683fa2a2c90043cd08ae0c89
xiaoyutianL <128241333+svip-leipeng@users.noreply.github.com>
2023-12-16 11:47:15 +0800

    Update WangzheSkin.py
    加入浏览器代理，每下载完一个英雄更换一个浏览器代理头

--- WangzheSkin.py
+++ WangzheSkin.py
@@ -2,46 +2,71 @@ import os
 import requests
 import time
 import random
-from tkinter import *
-from tkinter import filedialog
+from fake_useragent import UserAgent
+import logging
 
-url = 'https://pvp.qq.com/web201605/js/herolist.json'
-herolist = requests.get(url)
-herolist_json = herolist.json()
-hero_name = list(map(lambda x: x['cname'], herolist_json))
-hero_number = list(map(lambda x: x['ename'], herolist_json))
+# 设置日志记录
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def downloadPic():
-    i = 0
-    for j in hero_number:
-        os.makedirs("C:\\Users\\XXXXX\\XXXX\\skin\\" + hero_name[i])
-        os.chdir("C:\\Users\\XXXXX\\XXXX\\skin\\" + hero_name[i])
-        i += 1
-        for k in range(10):
-            onehero_link = 'http://game.gtimg.cn/images/yxzj/img201606/skin/hero-info/' + str(j) + '/' + str(j) + '-bigskin-' + str(k) + '.jpg'
-            im = requests.get(onehero_link)
-            if im.status_code == 200:
-                open(str(k) + '.jpg', 'wb').write(im.content)
-            time.sleep(random.randint(10, 20))
 
-def select_folder():
-    folder_path = filedialog.askdirectory()
-    return folder_path
+def create_directory(directory_path):
+    """
+    创建目录，如果目录已存在，则不执行任何操作。
+    """
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
 
-root = Tk()
-root.title("英雄皮肤下载器")
 
-label = Label(root, text="请选择要下载的英雄：")
-label.pack()
+def download_image(hero_name, hero_number, image_index):
+    """
+    下载图片到指定文件夹。
+    """
+    onehero_link = f'http://game.gtimg.cn/images/yxzj/img201606/skin/hero-info/{hero_number}/{hero_number}-bigskin-{image_index}.jpg'
+    ua = UserAgent()
+    headers = {'User-Agent': ua.random}
 
-for i, name in enumerate(hero_name):
-    button = Radiobutton(root, text=name, variable=StringVar(), value=i)
-    button.pack()
+    try:
+        im = requests.get(onehero_link, headers=headers, timeout=10)
+        if im.status_code == 200:
+            file_path = os.path.join(hero_name, f'{image_index}.jpg')
+            with open(file_path, 'wb') as f:
+                f.write(im.content)
+            logging.info(f"成功下载图片 {image_index}.jpg")
+        else:
+            logging.warning(f"下载失败，状态码：{im.status_code}")
+    except requests.exceptions.RequestException as e:
+        logging.error(f"下载请求出错：{e}")
 
-folder_button = Button(root, text="选择保存文件夹", command=select_folder)
-folder_button.pack()
 
-start_button = Button(root, text="开始下载", command=downloadPic)
-start_button.pack()
+def download_pic(hero_name, hero_number):
+    """
+    对每个英雄，下载其皮肤图片。
+    """
+    for k in range(10):
+        download_image(hero_name, hero_number, k)
+        time.sleep(random.randint(10, 20))
 
-root.mainloop()
+
+def main():
+    url = 'https://pvp.qq.com/web201605/js/herolist.json'
+    try:
+        herolist_response = requests.get(url, timeout=10)
+        herolist_json = herolist_response.json()
+    except requests.exceptions.RequestException as e:
+        logging.error(f"获取英雄列表失败：{e}")
+        return
+
+    skin_root_directory = "D:/Project/test]/skin"
+    create_directory(skin_root_directory)
+
+    for i, hero in enumerate(herolist_json):
+        hero_name = hero['cname']
+        hero_number = hero['ename']
+        hero_directory = os.path.join(skin_root_directory, hero_name)
+        create_directory(hero_directory)
+        download_pic(hero_name, hero_number)
+        logging.info(f"已完成 {hero_name} 的皮肤下载")
+
+
+if __name__ == "__main__":
+    main()
